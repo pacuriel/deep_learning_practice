cbecfffc38809547235751718cd0b6d410bf0f56
Pablo Curiel <pacuriel@csuchico.edu>
2024-02-24 19:49:27 -0800

    init commit

---
+++ simple_fullnet.py
@@ -0,0 +1,22 @@
+#imports
+import torch #entire Pytorch library
+import torch.nn as nn #NN modules 
+import torch.optim as optim #optimization algos (SGD, etc.)
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+#create fully connected network
+
+#set device
+
+#load data
+
+#initialize network
+
+#loss and optimizer
+
+#train network
+
+#check accuracy on training and test data
